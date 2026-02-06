af3aaf01bf9a24cf679fd79ff42a3ac03e99b451
unknown <edwardariatanujaya@gmail.com>
2026-02-06 21:01:05 +0700

    feat:creating template for frontend

---
+++ splitit-frontend/src/store/UserAuthStore.ts
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { create } from "zustand";
+import type { UserAuthType } from "../type/UserAuthType";
+
+const useUserAuth = create<UserAuthType>((set, get) => ({
+  //initial value of the user
+  user: null,
+  authChecked: false,
+
+  register: async (name, email, password) => {
+    try {
+      //create user infomation in db
+      await axios.post(`${import.meta.env.VITE_API_BASE_URL}/auth/register`, {
+        name,
+        email,
+        password,
+      });
+    } catch (error: any) {
+      //error handling
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 409) {
+          throw new Error("Email or username already exist");
+        }
+        throw new Error(error.response?.data?.message || "registration failed");
+      }
+      throw error;
+    }
+  },
+
+  login: async (userEmail, password) => {
+    // Fetching and storing token
+    await axios.post(
+      `${import.meta.env.VITE_API_BASE_URL}/auth/login`,
+      {
+        email: userEmail,
+        password,
+      },
+      { withCredentials: true }
+    );
+  },
+
+  fetchProfile: async () => {
+    try {
+      const userRes = await axios.get(
+        `${import.meta.env.VITE_API_BASE_URL}/auth/profile`,
+        {
+          withCredentials: true,
+        }
+      );
+      const { user } = userRes.data;
+      // console.log(user);
+      set({ user });
+    } catch (error) {
+      set({ user: null }); // Not authenticated
+    } finally {
+      set({ authChecked: true });
+    }
+  },
+
+  //use for protectedRoute
+  isAuth: () => !!get().user, // double negation return the boolean value
+
+  //remove all the user data information
+  logout: async () => {
+    set({ user: null });
+    await axios.post(
+      `${import.meta.env.VITE_API_BASE_URL}/auth/logout`,
+      {},
+      { withCredentials: true }
+    );
+  },
+}));
+
+export default useUserAuth;
