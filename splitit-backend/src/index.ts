996e08e784734a6ee0b8b5d5bc430308bacafe0f
unknown <edwardariatanujaya@gmail.com>
2026-02-12 11:24:10 +0700

    fix:fix backend architecture

---
+++ splitit-backend/src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
